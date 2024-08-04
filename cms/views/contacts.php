4e29815cfc8004635405f2f99fc957fa44c0e495
Robin Boers <robindev2019@outlook.com>
2024-08-04 22:14:36 +0200

    Make contacts page look decent

--- cms/views/contacts.php
+++ cms/views/contacts.php
@@ -3,18 +3,19 @@
   <a href="<?= CMS_CANONICAL ?>/contacts/add" class="button">Add contact</a>
 </header>
 
+<ul>
+  <?php foreach(\store\list_contacts() as $contact) { ?>
+    <li>
+      <a href="//<?= $contact['domain'] ?>">@<?= $contact['handle'] ?></a>
 
-<?php foreach(\store\list_contacts() as $contact) { ?>
-  <li>
-    <a href="//<?= $contact['domain'] ?>">@<?= $contact['handle'] ?></a>
-
-    <span class="actions">
-      <a href="mailto:<?= $contact['email'] ?>">
-        Contact
-      </a>
-      <a href="<?= CMS_CANONICAL ?>/contacts/edit?id=<?= $contact['id'] ?>">
-        Edit
-      </a>
-    </span>
-  </li>
-<?php } ?>
+      <span class="actions">
+        <a href="mailto:<?= $contact['email'] ?>">
+          Send message
+        </a>
+        <a href="<?= CMS_CANONICAL ?>/contacts/edit?id=<?= $contact['id'] ?>">
+          Edit
+        </a>
+      </span>
+    </li>
+  <?php } ?>
+</ul>
