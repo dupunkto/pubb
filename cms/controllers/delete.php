5f143945c4afbcc88a6642b7f157a376de59df72
Robin Boers <robindev2019@outlook.com>
2024-08-11 18:15:46 +0200

    Unify page deletion

--- cms/controllers/delete.php
+++ cms/controllers/delete.php
@@ -1,10 +1,14 @@
 <?php
-// Delete a post.
+// Delete a page/gist/post.
 
-if(!isset($_GET['id'])) redirect("/pages");
+$type = @$_GET['type'] ?? "page";
+$return = @$_GET['return'] ?? "/pages";
+
+if(!isset($_GET['id'])) redirect($return);
 
 $id = $_GET['id'];
-$page = \store\get_page($id) or fail("Page doesn't exist.", to: "/pages");
-\store\delete_page($id) or fail("Couldn't delete page.", to: "/pages");
+$page = \store\get_page($id) or fail(ucfirst($type) . " doesn't exist.", to: $return);
+\store\delete_page($id) or fail("Couldn't delete $type.", to: $return);
 
-complete("Deleted " . \html\page_title($page) . ".", to: "/pages");
+$title = \html\page_title($page);
+complete("Deleted '{$title}'.", to: $return);
