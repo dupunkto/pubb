4c3d924fe65e9c9a38fe3a4b416378b80e8e3bd1
Robin <robindev2019@outlook.com>
2024-09-03 08:24:47 +0200

    Attempt to fix IndieAuth endpoint discovery

--- site/headers.php
+++ site/headers.php
@@ -10,7 +10,13 @@ header("X-Frame-Options: deny");
 header("Cross-Origin-Opener-Policy: same-origin");
 
 // See https://ncase.me/nutshell/#CORSForWebDevs
-header("Access-Control-Allow-Origin: *"); 
+header("Access-Control-Allow-Origin: *");
+
+// Makes IndieAuth work on sites with *weird* discovery
+header('Link: <' . AUTH_ENDPOINT . '>; rel="authorization_endpoint"');
+header('Link: <' . TOKEN_ENDPOINT . '>; rel="token_endpoint"');
+header('Link: <' . MICROPUB_ENDPOINT . '>; rel="micropub"');
+header('Link: <' . CANONICAL . '>; rel="self"');
 
 // Shameless self-promotion
 header("X-Powered-By: Pubb (v" . PUBB_VERSION . ")");
