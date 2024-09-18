a4b709159f420199a0e079ddaf8e0dd2f1a6baa9
Robin <robindev2019@outlook.com>
2024-09-18 22:04:06 +0200

    Respect DNT header and do not record views

--- core/stats.php
+++ core/stats.php
@@ -5,10 +5,16 @@
 namespace stats;
 
 function record_view($path) {
+  if(!allowed_to_track()) return;
+
   \store\put_view(
     path: $path, 
     referer: @$_SERVER['HTTP_REFERER'],
     agent: @$_SERVER['HTTP_USER_AGENT'],
     datetime: date("Y-m-d H:i:s")
   );
+}
+
+function allowed_to_track() {
+   return !isset($_SERVER['HTTP_DNT']) || $_SERVER['HTTP_DNT'] != 1;
 }
