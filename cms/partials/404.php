0a9e9a69f5a6c0a43697b22fd71dacb90de7fcc7
Robin <robindev2019@outlook.com>
2025-08-26 14:51:39 +0200

    Better 404

--- cms/partials/404.php
+++ cms/partials/404.php
@@ -1,1 +1,5 @@
-<h2>Whoops, that's a 404!</h2>
+<style><?php include(path_join($styles, "404.css")) ?></style>
+
+<h2>womp womp</h2>
+
+<p>i am sorry to inform you of the unfortunate fact that the page you are looking for appears to have been eaten by a crocodile. though luck.</p>
