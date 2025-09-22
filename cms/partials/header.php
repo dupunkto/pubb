d55a059ee0887b02d0d5f4fdc040ac4360b53431
Robin <robindev2019@outlook.com>
2025-09-22 23:52:26 +0200

    Add special rendering for running via Scatter
    When running in scatter, the top bar shows a link back to the main scatter dashboard, rather than 'trapping' the user inside the pebble instance. This is of course entirely optional and serves as a progressive enhancement.

--- cms/partials/header.php
+++ cms/partials/header.php
@@ -1,5 +1,9 @@
 <header>
-  <h1><a href="<?= CMS_CANONICAL ?>">pebble</a></h1>
+  <?php if(defined('COCKPIT')) { ?>
+    <h1><a href="<?= PROTO ?>://<?= COCKPIT ?>">Scatter</a></h1>
+  <?php } else { ?>
+    <h1><a href="<?= CMS_CANONICAL ?>">pebble</a></h1>
+  <?php } ?>
 
   <?php if(function_exists('is_authenticated') and is_authenticated()) { ?>
     <div class="group">
