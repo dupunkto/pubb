86aa8f5b184984014c50bba43541f9106138b77c
Robin <robindev2019@outlook.com>
2025-12-09 20:43:52 +0100

    Fix IndieAuth implementation

--- config.php
+++ config.php
@@ -86,7 +86,7 @@ fallback('pingback-endpoint',
 fallback('cms.host', "cms." . HOST);
 fallback('cms.canonical', PREFERED_PROTO . "://" . CMS_HOST);
 
-resolute('issuer', CANONICAL . "/");
+resolute('issuer', CMS_CANONICAL . "/");
 resolute('client-id', ISSUER);
 resolute('redirect-uri', CMS_CANONICAL . "/auth");
 resolute('supported-scopes', ["create", "update", "delete", "media"]);
