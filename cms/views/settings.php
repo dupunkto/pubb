7c89c434dae76b268a291fd156bea7747aa9a77e
Robin <robindev2019@outlook.com>
2025-12-22 00:20:27 +0100

    Reorder settings

--- cms/views/settings.php
+++ cms/views/settings.php
@@ -323,6 +323,20 @@
   </table>
 
   <p>
+    <input type="hidden" name="feeds.updated-top" value="false" />
+
+    <label>
+      <input
+        type="checkbox"
+        name="feeds.updated-top"
+        <?php if(FEEDS_UPDATED_TOP) echo "checked" ?>
+        value="true"
+      >
+      <span>Updated posts rise to the top</span>
+    </label>
+  </p>
+
+  <p>
     <label>Feeds</label>
     <span>Select categories to be included in RSS, Atom and JSON feeds.</span>
   </p>
@@ -341,20 +355,6 @@
         <span><?= $title ?></span>
       </label>
     <?php } ?>
-  </p>
-
-  <p>
-    <input type="hidden" name="feeds.updated-top" value="false" />
-
-    <label>
-      <input
-        type="checkbox"
-        name="feeds.updated-top"
-        <?php if(FEEDS_UPDATED_TOP) echo "checked" ?>
-        value="true"
-      >
-      <span>Updated posts rise to the top</span>
-    </label>
   </p>
 
   <h3>Advanced</h3>
