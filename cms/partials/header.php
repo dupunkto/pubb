88223dea390433e185a43665826fb738b476c73e
Robin <robindev2019@outlook.com>
2026-04-25 20:38:29 +0200

    Remove old Scatter integration code

--- cms/partials/header.php
+++ cms/partials/header.php
@@ -1,14 +1,10 @@
 <header>
-  <?php if(defined('COCKPIT')) { ?>
-    <h1><a href="<?= PROTO ?>://<?= COCKPIT ?>">Scatter</a></h1>
-  <?php } else { ?>
-    <h1><a href="<?= CMS_CANONICAL ?>">pebble</a></h1>
-  <?php } ?>
+  <h1><a href="<?= CMS_CANONICAL ?>">pebble</a></h1>
 
   <?php if(function_exists('is_authenticated') and is_authenticated()) { ?>
-    <div class="group">
+    <nav class="group">
       <a href="<?= CANONICAL ?>" target="_blank">View site →</a>
       <a href="<?= CMS_CANONICAL ?>/settings">Settings</a>
-    </div>
+    </nav>
   <?php } ?>
 </header>
